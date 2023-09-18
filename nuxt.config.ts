4fd6d18ab0477e31377cb49ba4a106cf5447034d
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-09-18 13:05:04 +0900

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["nuxt-microcms-module"],
-  microCMS: {
-    serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
-    apiKey: process.env.MICROCMS_API_KEY,
-  },
+  modules:  [
+    ['nuxt-microcms-module', {
+		serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+		apiKey: process.env.MICROCMS_API_KEY,
+    }]
+  ],
   devtools: { enabled: true },
   build: {
     transpile: ['gsap']

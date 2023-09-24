98e13bc5ee955f30a693f35a3161acf4280eb6ae
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-09-24 10:42:15 +0900

    update contents

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,9 +1,27 @@
 <template>
     <main class="under-main">
-        <section class="">
+        <section class="skills">
             <div class="bg"></div>
             <h1>skills</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio distinctio optio quaerat error esse deleniti quos velit vitae ad pariatur voluptate iste officia laborum, nobis unde delectus libero consectetur dolorum!</p>
+            <h2>My Journey in Tech</h2>
+
+            <p>When I first set foot in the world of programming, my starting point was JavaScript. Over the years, my technical prowess has evolved exponentially, mastering a vast array of frameworks and languages.</p>
+
+            <h2>Static Site Generation with Astro</h2>
+            <p>I've honed my skills in Astro to craft fast, SEO-optimized websites. This framework excels in static site generation, and I know how to maximize its potential.</p>
+
+            <h2>Server-Side Rendering with Next.js and Nuxt.js</h2>
+            <p>Using these frameworks, I deliver efficient server-side rendering, providing a seamless browsing experience for users.</p>
+
+            <h2>Back-End Development with Python and Django</h2>
+            <p>Building efficient and secure back-end systems is one of my core skills. I develop robust systems using Python and Django.</p>
+
+            <h2>Design and Prototyping with Newt and Figma</h2>
+            <p>Beyond building systems, I'm also well-versed in the design process. Using Newt and Figma, I design captivating UI/UX and create prototypes that facilitate feedback sessions with clients and team members.</p>
+
+            <p>These are but a glimpse of the skills I possess. In a world that continually evolves with technology, I consistently strive to keep abreast of the latest trends and techniques, ensuring that I deliver the best results in every project.</p>
+
+			<Cta />
         </section>
 	</main>
 </template>

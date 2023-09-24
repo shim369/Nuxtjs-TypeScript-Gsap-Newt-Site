9b6deb5a7ce2048bd9326b0fe2bf0b46932304c3
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-09-24 10:31:37 +0900

    update css

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer>
-        <div class="footer-logo"><NuxtLink to="/">Nuxt Base</NuxtLink></div>
-    </footer>
+	<footer>
+		<div class="footer-logo"><NuxtLink to="/">Nuxt Base</NuxtLink></div>
+	</footer>
 </template>

e62a58d23df1fbc091d8b50f3bd46612fc6a0319
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-09-30 11:05:25 +0900

    update css

--- pages/about.vue
+++ pages/about.vue
@@ -2,10 +2,7 @@
     <main class="under-main">
         <section class="about">
             <div class="bg"></div>
-            <div class="under-head">
             <h1>about</h1>
-
-            </div>
             <h2>Elevate Your Web Development with Nuxt 3</h2>
             <p class="read-text">Web development has been evolving rapidly, and in this era of change, Nuxt 3 emerges as an avant-garde framework that empowers developers to craft impeccable web experiences.
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio distinctio optio quaerat error esse deleniti quos velit vitae ad pariatur voluptate iste officia laborum, nobis unde delectus libero consectetur dolorum!</p>
@@ -14,19 +11,37 @@
 
             <h2>Why Nuxt 3?</h2>
 
-            <div class="reason-box">
-                <ul>
-                    <li><span class="highlight">Built on Vue 3</span>Nuxt 3 inherits all the advantages of Vue 3. This means developers can utilize the reactivity system, composition API, and more, resulting in cleaner and more efficient code.</li>
-                    <li><span class="highlight">High Performance</span>Speed matters. Nuxt 3 ensures that your web applications run smoothly, providing an optimal user experience. With server-side rendering (SSR), your application starts faster, improving SEO and user engagement.</li>
-                    <li><span class="highlight">Efficient Development Support</span>With features such as hot module replacement and a comprehensive development environment, developers can quickly build, test, and iterate on their projects.</li>
-                    <li><span class="highlight">Automatic Routing</span>Forget the hassle of manually configuring routes. Nuxt 3 auto-generates them based on your file structure. This not only simplifies the development process but also reduces the risk of errors.</li>
-                    <li><span class="highlight">SSR & SSG Capabilities</span>Whether you need a dynamic application with Server-Side Rendering (SSR) or a lightning-fast static site with Static Site Generation (SSG), Nuxt 3 has got you covered. It enables developers to decide the best approach for their projects.</li>
-                    <li><span class="highlight">Customization & Extension</span>Nuxt 3 is designed to be flexible. With its modular architecture, you can easily customize and extend the framework to suit your needs. Plugins, modules, and community-contributed extensions ensure that you have all the tools at your disposal.</li>
-                    <li><span class="highlight">Vibrant Community & Ecosystem</span>One of Nuxt 3's greatest strengths is its community. From novice developers to experts, there's a passionate group of individuals constantly contributing to its growth. With a plethora of plugins, modules, and tutorials available, you're never alone in your development journey.</li>
-                </ul>
-
-                <p>In a nutshell, Nuxt 3 is not just a framework; it's a movement towards a more efficient, performative, and seamless web development experience. Whether you're building a personal blog, an e-commerce site, or a large-scale web application, Nuxt 3 provides a robust foundation to ensure success.</p>
+            <div class="flex-box">
+                <div class="flex-item">
+                    <h2>Built on Vue 3</h2>
+                    <p>Nuxt 3 inherits all the advantages of Vue 3. This means developers can utilize the reactivity system, composition API, and more, resulting in cleaner and more efficient code.</p>
+                </div>
+                <div class="flex-item">
+                    <h2>High Performance</h2>
+                    <p>Speed matters. Nuxt 3 ensures that your web applications run smoothly, providing an optimal user experience. With server-side rendering (SSR), your application starts faster, improving SEO and user engagement.</p>
+                </div>
+                <div class="flex-item">
+                    <h2>Efficient Development Support</h2>
+                    <p>With features such as hot module replacement and a comprehensive development environment, developers can quickly build, test, and iterate on their projects.</p>
+                </div>
+                <div class="flex-item">
+                    <h2>Automatic Routing</h2>
+                    <p>Forget the hassle of manually configuring routes. Nuxt 3 auto-generates them based on your file structure. This not only simplifies the development process but also reduces the risk of errors.</p>
+                </div>
+                <div class="flex-item">
+                    <h2>SSR & SSG Capabilities</h2>
+                    <p>Whether you need a dynamic application with Server-Side Rendering (SSR) or a lightning-fast static site with Static Site Generation (SSG), Nuxt 3 has got you covered. It enables developers to decide the best approach for their projects.</p>
+                </div>
+                <div class="flex-item">
+                    <h2>Customization & Extension</h2>
+                    <p>Nuxt 3 is designed to be flexible. With its modular architecture, you can easily customize and extend the framework to suit your needs. Plugins, modules, and community-contributed extensions ensure that you have all the tools at your disposal.</p>
+                </div>
+                <div class="flex-item">
+                    <h2>Vibrant Community & Ecosystem</h2>
+                    <p>One of Nuxt 3's greatest strengths is its community. From novice developers to experts, there's a passionate group of individuals constantly contributing to its growth. With a plethora of plugins, modules, and tutorials available, you're never alone in your development journey.</p>
+                </div>
             </div>
+            <p>In a nutshell, Nuxt 3 is not just a framework; it's a movement towards a more efficient, performative, and seamless web development experience. Whether you're building a personal blog, an e-commerce site, or a large-scale web application, Nuxt 3 provides a robust foundation to ensure success.</p>
 			<Cta />
         </section>
 	</main>
--- pages/skills.vue
+++ pages/skills.vue
@@ -7,31 +7,31 @@
 
             <p class="read-text">When I first set foot in the world of programming, my starting point was JavaScript. Over the years, my technical prowess has evolved exponentially, mastering a vast array of frameworks and languages.</p>
 
-            <div class="skills-box">
-                <div class="skills-item">
+            <div class="flex-box">
+                <div class="flex-item">
                     <h2>Static Site Generation with Astro</h2>
                     <p>I've honed my skills in Astro to craft fast, SEO-optimized websites. This framework excels in static site generation, and I know how to maximize its potential.</p>
                 </div>
 
-                <div class="skills-item">
+                <div class="flex-item">
                     <h2>Next.js Efficient Server-Side Rendering</h2>
                     <p>With Next.js, I deliver efficient server-side rendering, providing a seamless browsing experience for users.</p>
                 </div>
 
-                <div class="skills-item">
+                <div class="flex-item">
                     <h2>Nuxt.js Powerful Server-Side Rendering</h2>
                     <p>Using Nuxt.js, I ensure powerful server-side rendering, resulting in a smooth and uninterrupted browsing experience for users.</p>
                 </div>
 
-                <div class="skills-item">
+                <div class="flex-item">
                     <h2>Back-End Development with Python and Django</h2>
                     <p>Building efficient and secure back-end systems is one of my core skills. I develop robust systems using Python and Django.</p>
                 </div>
-                <div class="skills-item">
+                <div class="flex-item">
                     <h2>Newt Blog and Contact Features</h2>
                     <p>Newt serves as the backbone of our website, enabling the seamless integration of blog and contact features. This simplifies content management and streamlines website development.</p>
                 </div>
-                <div class="skills-item">
+                <div class="flex-item">
                     <h2>UI/UX Design and Prototyping with Figma</h2>
                     <p>Figma plays a crucial role in the design process. I excel in system development, UI/UX design, and creating prototypes for client and team feedback.</p>
                 </div>

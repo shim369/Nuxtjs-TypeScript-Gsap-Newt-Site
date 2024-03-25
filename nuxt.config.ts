292c92f1b5538c47cf151acb05f5d0c960437ab5
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-03-25 18:40:53 +0900

    test aos

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
     }
   },
   plugins: [
-    { src: '@/plugins/aos.d.ts', ssr: false, mode: "client" }
+    { src: '@/plugins/aos.ts', ssr: false, mode: "client" }
   ],
   typescript: {
     typeCheck: true
